d080f35951eff2cf56c68edf0b9c7cfe90f4c6ce
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-07-01 15:53:06 -0700

    add script so newly added custom group is automatically selected

--- Content/JS/scripts.js
+++ Content/JS/scripts.js
@@ -3,12 +3,13 @@
 
 
   $('#addGroup').click(function() {
-    $('#newGroupForm').toggleClass('hidden');
+    $('#newGroupForm, #addGroup').toggleClass('hidden');
   });
   $('#newGroupForm').submit(function(event) {
     event.preventDefault();
     var newGroup = $('#newGroup').val();
     $('#group').append("<option value='" + newGroup + "'>" + newGroup + "</option>");
     $('#newGroup').val('');
-    $('#newGroupForm').toggleClass('hidden');
+    $('#newGroupForm, #addGroup').toggleClass('hidden');
+    $('#group option:contains(' + newGroup + ')').prop('selected', true);
   });
